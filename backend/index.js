a3dd105d11f566ebca1f145b1a57fa01db41d005
John Soukka <john@knodd.se>
2023-11-09 06:36:13 +0100

    Update deps to latest

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,10 @@ const DB_URL = process.env.DB_HOST || "mongodb://127.0.0.1/image-app-db";
 const PORT = process.env.PORT || 3002;
 
 // Setup db connection
-mongoose.connect(
-  DB_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (err) {
-    if (err) {
-      console.error("Error occurred while connecting to DB!");
-    } else {
-      console.log("Database connection established successfully");
-    }
-  }
-);
+mongoose
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connection established successfully"))
+  .catch((err) => console.error("Error occurred while connecting to DB!"));
 
 // Run the server
 app.listen(PORT, () => {
