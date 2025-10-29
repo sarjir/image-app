59ec88fed894dced7240c3d9fa6c458735fb77d0
Sara Kent <sara@weapp.se>
2025-10-29 20:55:52 +0100

    Add inputs for frontend

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 import { App } from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const title = getByText(/Image Uploading App/i);
+test("renders upload image form", () => {
+  render(<App />);
+
+  const title = screen.getByText(/Image Uploading App/i);
+  const fileInput = screen.getByLabelText("Upload an image:");
+  const nameInput = screen.getByRole("textbox");
+  const submitButton = screen.getByRole("button", { name: /Submit/i });
+
   expect(title).toBeInTheDocument();
+  expect(fileInput).toBeInTheDocument();
+  expect(nameInput).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,13 @@ export const App = () => {
       <header className="App-header">
         <h1>Image Uploading App</h1>
       </header>
+      <main>
+        <label htmlFor="fileInput">Upload an image:</label>
+        <input id="fileInput" type="file" accept="image/png, image/jpeg" />
+        <label htmlFor="nameInput">Image Name:</label>
+        <input type="text" placeholder="Enter image name" />
+        <button type="submit">Submit</button>
+      </main>
     </div>
   );
 };
